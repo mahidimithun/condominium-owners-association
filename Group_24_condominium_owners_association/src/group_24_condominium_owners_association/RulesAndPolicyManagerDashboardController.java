2d3838b383f07c33366d06fef074eda457a611e8
Monosijkantisarker-1234 <1930374@iub.edu.bd>
2024-03-05 13:21:03 +0600

    add securitycontrolofficerdashboard

---
+++ Group_24_condominium_owners_association/src/group_24_condominium_owners_association/RulesAndPolicyManagerDashboardController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package group_24_condominium_owners_association;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS
+ */
+public class RulesAndPolicyManagerDashboardController implements Initializable {
+
+    @FXML
+    private TextField tf_visitorName;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void showVisitorListOnButtonClick(ActionEvent event) {
+    }
+    
+}
