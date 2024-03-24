0e2fc2de477962fb90413d1684d243972fd0341c
unknown <mahidi001hasan@gmail.com>
2024-03-24 20:29:36 +0600

    secence change unit owners

--- Group_24_condominium_owners_association/src/group_24_condominium_owners_association/LogInUIController.java
+++ Group_24_condominium_owners_association/src/group_24_condominium_owners_association/LogInUIController.java
@@ -4,15 +4,21 @@
  */
 package group_24_condominium_owners_association;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -42,7 +48,22 @@ public class LogInUIController implements Initializable {
     }
 
     @FXML
-    private void logInOnClick(ActionEvent event) {
+    private void logInOnClick(ActionEvent event) throws IOException{
+//        Not parmanet it's for only Demonstration
+    
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("UnitOwnerDashboard.fxml")); 
+        Parent parent = loader.load();
+
+       Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        Scene studentScene = new Scene(parent);
+
+        currentStage.setScene(studentScene); 
+        currentStage.show();
+        
+//       Alret Its not parment 
+        
+        
     }
     
 }
--- Group_24_condominium_owners_association/src/group_24_condominium_owners_association/UnitOwnerDashboardController.java
+++ Group_24_condominium_owners_association/src/group_24_condominium_owners_association/UnitOwnerDashboardController.java
@@ -4,12 +4,18 @@
  */
 package group_24_condominium_owners_association;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -27,22 +33,47 @@ public class UnitOwnerDashboardController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-
-    @FXML
-    private void submitComplainOnClick(ActionEvent event) {
     }
 
     @FXML
-    private void paymentOnClick(ActionEvent event) {
+    private void submitComplainOnClick(ActionEvent event)  {
+        
     }
 
     @FXML
-    private void extraServicesOnClick(ActionEvent event) {
+    private void paymentOnClick(ActionEvent event) throws IOException {
+
+        
     }
 
     @FXML
-    private void bookingOnClick(ActionEvent event) {
+    private void extraServicesOnClick(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("UwExtraServices.fxml"));
+        Parent parent = loader.load();
+
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        Scene studentScene = new Scene(parent);
+
+        currentStage.setScene(studentScene);
+        currentStage.show();
     }
-    
+
+    @FXML
+    private void bookingOnClick(ActionEvent event) throws IOException{
+        
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("UwGardeningBooking.fxml"));
+        Parent parent = loader.load();
+
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        Scene studentScene = new Scene(parent);
+
+        currentStage.setScene(studentScene);
+        currentStage.show();
+        
+    }
+
 }
